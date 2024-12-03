a33a14a8f8824d07eab463af9f027e61ae1a4b50
Andrey Larikov <aalarikov@avito.ru>
2024-12-04 01:05:44 +0500

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,3 +1,11 @@
+function isNumber(prop) {
+  return typeof prop === "number" && !isNaN(prop);
+}
+
 export default function sum(a, b) {
-  /* ваш код */
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError("Oops! Check type of incoming props");
+  }
+
+  return a + b;
 }
