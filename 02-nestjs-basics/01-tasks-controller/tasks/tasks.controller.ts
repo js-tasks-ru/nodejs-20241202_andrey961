c9b73b4eced3b1d2678dd7186a87252945675915
Andrey Larikov <aalarikov@avito.ru>
2024-12-06 11:27:52 +0500

    add solution for 2-module 1-task

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
@@ -1,31 +1,33 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from "@nestjs/common";
-import { TasksService } from "./tasks.service";
-import { Task } from "./task.model";
+import { Body, Controller, Delete, Get, Param, Patch, Post, } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {}
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
 
   @Get(":id")
-  getTaskById(@Param("id") id: string) {}
+  getTaskById(@Param("id") id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
 
   @Post()
-  createTask(@Body() task: Task) {}
+  createTask(@Body() task: Task): Task {
+    return this.tasksService.createTask(task);
+  }
 
   @Patch(":id")
-  updateTask(@Param("id") id: string, @Body() task: Task) {}
+  updateTask(@Param("id") id: string, @Body() task: Task): Task {
+    return this.tasksService.updateTask(id, task);
+  }
 
   @Delete(":id")
-  deleteTask(@Param("id") id: string) {}
+  deleteTask(@Param("id") id: string): Task {
+    return this.tasksService.deleteTask(id);
+  }
 }
