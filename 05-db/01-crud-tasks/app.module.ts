9037d58a0b10420bebd6444235a3bec1a72c0629
Andrey Larikov <aalarikov@avito.ru>
2025-01-03 01:46:15 +0500

    add solution for 5-module 1-task

--- 05-db/01-crud-tasks/app.module.ts
+++ 05-db/01-crud-tasks/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Task } from "./tasks/entities/task.entity";
 import { TasksModule } from "./tasks/tasks.module";
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: [Task],
+    synchronize: true,
+  })],
 })
 export class AppModule {}
--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  BadRequestException,
+  Query,
+} from "@nestjs/common";
+import { DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_LIMIT } from "./dto/constants";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 import { TasksService } from "./tasks.service";
 
 @Controller("tasks")
@@ -6,17 +19,42 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create() {}
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto);
+  }
 
   @Get()
-  findAll() {}
+  findAll(@Query("page") page: string, @Query("limit") limit: string) {
+    const pageNumber = page ? parseInt(page, 10) : DEFAULT_PAGE_NUMBER;
+    const limitNumber = limit ? parseInt(limit, 10) : DEFAULT_PAGE_LIMIT;
+
+    return this.tasksService.findAll(pageNumber, limitNumber);
+  }
 
   @Get(":id")
-  findOne() {}
+  findOne(@Param("id") id: string) {
+    if (isNaN(+id)) {
+      throw new BadRequestException("id must be an integer");
+    }
+
+    return this.tasksService.findOne(+id);
+  }
 
   @Patch(":id")
-  update() {}
+  update(@Param("id") id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    if (isNaN(+id)) {
+      throw new BadRequestException("id must be an integer");
+    }
+
+    return this.tasksService.update(+id, updateTaskDto);
+  }
 
   @Delete(":id")
-  remove() {}
+  remove(@Param("id") id: string) {
+    if (isNaN(+id)) {
+      throw new BadRequestException("id must be an integer");
+    }
+
+    return this.tasksService.remove(+id);
+  }
 }
